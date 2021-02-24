446dc98a821ac848ce60cc346121fc17157d126d
Dusan Vostic <vosticdusan@gmail.com>
2021-02-24 09:26:22 +0100

    Created Movie model/entity with basic movie structure

--- movies/api/serializers.py
+++ movies/api/serializers.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.Serializer):
   username = serializers.CharField(max_length = 128)
-  password = serializers.CharField(max_length = 128, write_only = True)
+  password = serializers.CharField(max_length = 128, min_length=6, write_only = True)
   email = serializers.EmailField(max_length = 128)
 
   class Meta:
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ('ACTION', 'Action'),
+        ('DRAMA', 'Drama'),
+        ('HOROR', 'Horor'),
+        ('ROMANCE', 'Romance'),
+        ('COMEDY', 'Comedy'),
+        ('THRILLER', 'Thriller'),
+        ('CRIME', 'Crime'),
+        ('WESTERN', 'Western'),
+    ]
+
+    movie_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    cover_image = models.URLField(null=False, blank=False)
+    genre = models.CharField(
+        max_length=32,
+        choices=GENRE_CHOICES,
+        default='ACTION',
+    )
+
+    def __str__(self):
+        return self.title
